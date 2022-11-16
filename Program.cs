6ff0658a0dcc8910e1cd8254a076b66162fca3ce
Mareusas <96440192+Mareusas@users.noreply.github.com>
2022-11-16 07:34:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Marius.Dobrovolskis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string readText = File.ReadAllText(@"C:\Users\r7hbs\Desktop\FAILAS.txt");
+
+            char[] charArr = readText.ToCharArray();
+
+            Console.WriteLine(charArr);
+
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+
+                int count = 0;
+
+
+                foreach (char c2 in charArr)
+
+                {
+
+                    if (i == c2)
+                        count++;
+                    if (i != c2)
+                        continue;
+                }
+
+                if (count == 1) { Console.Write(i); }
+
+                if (count > 1) { Console.Write($"{i}{count}"); }
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
+
